367fd36aad688e0bbf7089afadb603cc33846af3
Lamia BOUKROUH <lboukrou@e1r13p10.42.fr>
2020-01-04 04:20:22 +0100

    beginning norme

--- srcs/asm/parse_utils.c
+++ srcs/asm/parse_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toliver <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 15:48:43 by toliver           #+#    #+#             */
-/*   Updated: 2019/12/04 18:43:24 by toliver          ###   ########.fr       */
+/*   Updated: 2020/01/04 04:04:29 by lboukrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ void		ft_skip_spaces(t_env *env, t_file *file)
 		}
 		size += i;
 		if (i != retval)
-			break;
+			break ;
 	}
 	if (retval == -1)
 		ft_error(env, file, READ_ERROR);
@@ -84,6 +84,30 @@ void		ft_skip_spaces(t_env *env, t_file *file)
 		ft_offset_head(env, file, size);
 }
 
+// static int	read_file_fd(t_file *file, char *containing, char *buf)
+// {
+// 	int		retval;
+// 	int		size;
+// 	int		i;
+
+// 	size = 0;
+// 	while ((retval = read(file->fd, buf, 50)) > 0)
+// 	{
+// 		i = 0;
+// 		buf[retval] = '\0';
+// 		while (buf[i] && ft_is_one_of(buf[i], containing))
+// 			i++;
+// 		size += i;
+// 		if (i != retval)
+// 			break ;
+// 	}
+// 	if (retval == -1)
+// 		return (-1);
+// 	if (retval == 0)
+// 		file->mode = DONE;
+// 	return (size);
+// }
+
 int			ft_parse_while(t_file *file, char *containing, char **line)
 {
 	char	buf[51];
@@ -91,16 +115,18 @@ int			ft_parse_while(t_file *file, char *containing, char **line)
 	int		size;
 	int		retval;
 
+	// if ((size = read_file_fd(file, containing, buf)) == -1)
+	// 	return (ft_error(ft_get_env(), file, READ_ERROR));
 	size = 0;
 	while ((retval = read(file->fd, buf, 50)) > 0)
 	{
 		i = 0;
 		buf[retval] = '\0';
-		while(buf[i] && ft_is_one_of(buf[i], containing))
+		while (buf[i] && ft_is_one_of(buf[i], containing))
 			i++;
 		size += i;
 		if (i != retval)
-			break;
+			break ;
 	}
 	if (retval == -1)
 		return (ft_error(ft_get_env(), file, READ_ERROR));
@@ -121,8 +147,6 @@ int			ft_parse_while(t_file *file, char *containing, char **line)
 	return (1);
 }
 
-
-
 int			ft_parse_until(t_file *file, char *limit, char **line, int skipping)
 {
 	char	buf[51];
@@ -136,11 +160,11 @@ int			ft_parse_until(t_file *file, char *limit, char **line, int skipping)
 	{
 		i = 0;
 		buf[retval] = '\0';
-		while(buf[i] && !(end = ft_is_one_of(buf[i], limit)))
+		while (buf[i] && !(end = ft_is_one_of(buf[i], limit)))
 			i++;
 		size += i;
 		if (i != retval)
-			break;
+			break ;
 	}
 	if (retval == -1)
 		return (ft_error(ft_get_env(), file, READ_ERROR));
@@ -162,5 +186,3 @@ int			ft_parse_until(t_file *file, char *limit, char **line, int skipping)
 		ft_offset_lines(ft_get_env(), file, ((char[]){end, '\0'}));
 	return (1);
 }
-
-
