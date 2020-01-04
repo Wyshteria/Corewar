cc622d130b191c4dc8e7ea5c3cf2553dffd44401
Julia ATES <jates-@e1r6p4.42.fr>
2020-01-04 16:38:27 +0100

    81 test passed

--- srcs/asm/ft_program.c
+++ srcs/asm/ft_program.c
@@ -44,7 +44,7 @@ static int			ft_check_body(t_file *file, t_program *prog)
 	while (tmp)
 	{
 		if (!ft_pass_newline_comm(file, &tmp))
-			return (0);
+			return (file->mode != CONTAIN_ERRORS);
 		if (tmp->type == LABEL && (!tmp->next || (tmp->next->type != NEWLINE \
 		&& tmp->next->type != OPERATION && tmp->next->type != COMMENT)))
 			return (ft_syntax_error(file, tmp));
